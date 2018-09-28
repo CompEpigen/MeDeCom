afc7854a3a93f1ab14b91f320982d81285554f57
schmic05 <mscherer@mpi-inf.mpg.de>
2018-09-28 13:02:02 +0200

    FactorViz function: link LMC proportions to sample traits

--- R/contribution_interpretation.R
+++ R/contribution_interpretation.R
@@ -14,12 +14,12 @@
 #' run.trait.association
 #' 
 #' Computes test statistics for all possible group assignments of samples defined in \code{medecom.set} and \code{rnb.set} and stores
-#' heatmaps of p-values on the given location
+#' heatmaps of p-values on the given location for all CG Subsets, Ks and lambdas present in \code{medecom.set}
 #' 
 #' @param medecom.set An object of type \code{\link{MeDeComSet}} as the result of \code{\link{runMeDeCom}} containing LMCs and their
 #'                     proportions in the samples. The Set can contain multiple runs for different values of K and lambda.
 #' @param rnb.set An object of type \code{\link[RnBeads]{RnBSet-class}} containing methylation data and metadata for the same samples for which 
-#'                 \code{medecom.set} was computed.
+#'                 \code{medecom.set} was computed or a data.frame of sample annotations (ann_S)
 #' @param test.fun Test statistic used to compute p-values of differences between LMC contributions in pairwise sample comparisons.
 #'                  Defaults to \code{t.test}.
 #' @param plot.path Path to store the p-value heatmaps.
@@ -72,6 +72,58 @@ run.trait.association <- function(medecom.set,rnb.set,test.fun=t.test,plot.path=
   }
 }
 
+#' run.trait.association.single
+#' 
+#' Computes test statistics for all possible group assignments of samples defined in \code{medecom.set} and \code{rnb.set} and stores
+#' heatmaps of p-values on the given location only for a given CG Subset, K and lambda.
+#' 
+#' @param medecom.set An object of type \code{\link{MeDeComSet}} as the result of \code{\link{runMeDeCom}} containing LMCs and their
+#'                     proportions in the samples. The Set can contain multiple runs for different values of K and lambda.
+#' @param rnb.set An object of type \code{\link[RnBeads]{RnBSet-class}} containing methylation data and metadata for the same samples for which 
+#'                 \code{medecom.set} was computed or a data.frame of sample annotations (ann_S)
+#' @param cg_subset The cg_subset of interest
+#' @param K The selected value for number of LMCs (K)
+#' @param lambda The selected value of the regularizer (lambda)
+#' @param test.fun Test statistic used to compute p-values of differences between LMC contributions in pairwise sample comparisons.
+#'                  Defaults to \code{t.test}.
+#' @param plot.path Path to store the p-value heatmaps.
+#' @param figure.format Character describing the format in which plots should be stored on disk. Either \code{"pdf"} or \code{"png"}.
+#' 
+#' @details This function creates a new folder names \code{pdfs} at the location given by \code{plot.path} and stores a heatmap for
+#'           all possible Ks and lambdas defined in \code{medecom.set}. The p-values are produced by comparing the LMC contributions
+#'           in all sample comparisons defined by \code{\link[RnBeads]{rnb.sample.groups}} on \code{rnb.set}. The employed test statistic for
+#'           pariwise comparison can be specified by \code{test.fun}, for groups defining more than one group \code{\link{kruskal.test}}
+#'           is employed. P-values lower than 0.01 are added to the heatmap.
+#'           
+#' @author Michael Scherer
+#' 
+#' @export
+run.trait.association.single <- function(medecom.set,rnb.set,cg_subset=NULL,K=NULL,lambda=NULL,test.fun=t.test){
+  if(is.null(cg_subset)){
+    cg_subset <- medecom.set@parameters$cg_subsets[1]
+  }else if (!(cg_subset %in%  medecom.set@parameters$cg_subsets)){
+    stop("Specified value for cg_subset not in medecom.set")
+  }
+  if(is.null(K)){
+    K <- medecom.set@parameters$Ks[1]
+  }else if (!(K %in%  medecom.set@parameters$Ks)){
+    stop("Specified value for K not in medecom.set")
+  }
+  if(is.null(lambda)){
+    lambda <- medecom.set@parameters$lambdas[1]
+  }else if (!(lambda %in%  medecom.set@parameters$lambdas)){
+    stop("Specified value for lambda not in medecom.set")
+  }
+  ret.list <- list()
+  p.vals <- link.to.traits(medecom.set=medecom.set,cg_subset=s,K=K,lambda=lambda,rnb.set=rnb.set,test.fun=test.fun)
+  plot <- plot.p.val.heatmap(p.vals)
+  ret.list[["quantitative"]] <- plot
+  cors <- quantitative.trait.association(medecom.set=medecom.set,cg_subset=s,K=K,lambda=lambda,rnb.set=rnb.set)
+  plot <- plot.correlation.heatmap(cors)
+  ret.list[["qualitative"]] <- plot
+  return(ret.list)
+}
+
 #' link.to.traits
 #' 
 #' This routine performs a statistical test to determine if the difference in LMC contributions is different for the sample groups
@@ -83,7 +135,7 @@ run.trait.association <- function(medecom.set,rnb.set,test.fun=t.test,plot.path=
 #' @param K The K parameter, determining the number of LMCs to extract from \code{medecom.set}.
 #' @param lambda The lambda parameter, determining the regularization used for the LMCs in \code{medecom.set}.
 #' @param rnb.set An object of type \code{\link{RnBSet}} containing methylation data and metadata for the same samples for which 
-#'                 \code{medecom.set} was computed.
+#'                 \code{medecom.set} was computed or a data.frame of sample annotations (ann_S)
 #' @param test.fun Test statistic used to compute p-values of differences between LMC contributions in pairwise sample comparisons.
 #'                  Defaults to \code{t.test}.
 #'                  
@@ -99,6 +151,9 @@ run.trait.association <- function(medecom.set,rnb.set,test.fun=t.test,plot.path=
 #' @noRd
 
 link.to.traits <- function(medecom.set,cg_subset,K,lambda,rnb.set,test.fun=t.test){
+  if(!inherits(rnb.set,"RnBSet") && !is.data.frame(rnb.set)){
+    stop("Invalid value for rnb.set; needs to be RnBSet or data.frame")
+  }
   require("RnBeads")
   sample.grps <- rnb.sample.groups(rnb.set)
   props <- getProportions(medecom.set,cg_subset=cg_subset,K=K,lambda=lambda)
@@ -113,7 +168,11 @@ link.to.traits <- function(medecom.set,cg_subset,K,lambda,rnb.set,test.fun=t.tes
           test.fun(x[grp[[1]]],x[grp[[2]]])$p.val
         })
       }else if(length(grp)>2){
-        vec <- rep(NA,length(samples(rnb.set)))
+        if(inherits(rnb.set,"RnBSet")){
+          vec <- rep(NA,length(samples(rnb.set)))
+        }else{
+          vec <- rep(NA,ncol(rnb.set))
+        }
         for(name in names.traits){
           vec[grp[[name]]] <- name
         }
@@ -140,7 +199,7 @@ link.to.traits <- function(medecom.set,cg_subset,K,lambda,rnb.set,test.fun=t.tes
 #' @param K The K parameter, determining the number of LMCs to extract from \code{medecom.set}.
 #' @param lambda The lambda parameter, determining the regularization used for the LMCs in \code{medecom.set}.
 #' @param rnb.set An object of type \code{\link{RnBSet}} containing methylation data and metadata for the same samples for which 
-#'                 \code{medecom.set} was computed.
+#'                 \code{medecom.set} was computed or a data.frame of sample annotations (ann_S)
 #'                  
 #' @return A list with an element for each quantitative trait present in \code{rnb.set}'s phenotypic information.
 #' 
@@ -151,9 +210,16 @@ link.to.traits <- function(medecom.set,cg_subset,K,lambda,rnb.set,test.fun=t.tes
 #' @noRd
 
 quantitative.trait.association <- function(medecom.set,cg_subset,K,lambda,rnb.set){
+  if(!inherits(rnb.set,"RnBSet") && !is.data.frame(rnb.set)){
+    stop("Invalid value for rnb.set; needs to be RnBSet or data.frame")
+  }
   require("RnBeads")
   sample.grps <- names(rnb.sample.groups(rnb.set))
-  ph <- pheno(rnb.set)
+  if(inherits(rnb.set,"RnBSet")){
+    ph <- pheno(rnb.set)
+  }else{
+    ph <- rnb.set
+  }
   quant.traits <- colnames(ph)
   quant.traits <- quant.traits[!(quant.traits%in%sample.grps)]
   props <- getProportions(medecom.set,cg_subset=cg_subset,K=K,lambda=lambda)
@@ -246,4 +312,3 @@ plot.correlation.heatmap <- function(trait.res){
   }
   return(plot)
 }
-

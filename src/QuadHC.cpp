b24afe1b013ada6087ce7cda85e7056d3e41ccad
lutsik <pavlo.lutsik@gmail.com>
2020-03-03 23:55:14 +0100

    removed another omp reference

--- src/QuadHC.cpp
+++ src/QuadHC.cpp
@@ -515,9 +515,9 @@ void spawn_threads(double* G, double* W, double* A, ptrdiff_t k, int d, double* 
     double loss = 0.0;
     //#pragma omp parallel for private(j), reduction(+: loss)
     for(j = 0; j < d; j++){
-        int id = omp_get_thread_num();
+        //int id = omp_get_thread_num();
         
-        //int id = 0;
+        int id = 0;
         int res=QuadHC(G, W + j * k, A + j * k, k,
                 Hess + id * (k * k), beta + id * k,
                 x + id * k, x_old + id * k, g + id * k, g_old + id * k, dsct + id * k,

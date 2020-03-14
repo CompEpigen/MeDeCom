0334db1f20b4d50dce2bb9f2e736d64010e0b036
nikitaved <nikitavedeneev@gmail.com>
2020-03-14 13:58:04 -0400

    remove explit thread num setting in cppTAfact

--- src/cppTAfact.cpp
+++ src/cppTAfact.cpp
@@ -733,11 +733,7 @@ RcppExport SEXP cppTAfact(SEXP mDtSEXP, SEXP mTtinitSEXP, SEXP mAinitSEXP,
     ///* Prepare Eigen for multithreading */
     //Eigen::initParallel();
     ///* Make Eigen run in a single-thread mode */
-    auto nthreads = Eigen::nbThreads();
-    std::cout << "nthreads: " << nthreads << std::endl;
-    Eigen::setNbThreads(5);
-    std::cout << "nthreads: " << nthreads << std::endl;
-    std::cout << std::endl;
+    //Eigen::setNbThreads(1);
     
     /*
      * We have to set global variables after each call.
